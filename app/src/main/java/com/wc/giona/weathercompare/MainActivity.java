0bcf3c01185c1056e3e562f51f914c82d5de4c35
Unknown <fossatigiona@gmail.com>
2017-11-23 15:38:30 +0100

    0.0.8
    Locked screenOrientation, added Toast on refresh, created Settings xml and some minor changes.

--- app/src/main/java/com/wc/giona/weathercompare/MainActivity.java
+++ app/src/main/java/com/wc/giona/weathercompare/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -56,6 +57,7 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onClick(View v) {
                 try {
+                    Toast.makeText(getBaseContext(), "Updating weather!" , Toast.LENGTH_SHORT ).show();
                     owmInfo = fetchWeatherOwm.getJSONowm("Verona");
                     apixuInfo = fetchWeatherApixu.getJSONapixu("Verona");
                     wuInfo = fetchWeatherWu.getForecastJSONwu("verona");
---
+++ app/src/main/java/com/wc/giona/weathercompare/Settings.java
@@ -0,0 +1,8 @@
+package com.wc.giona.weathercompare;
+
+import android.app.Activity;
+
+public class Settings extends Activity {
+
+    protected void OnCreate(Bundle S)
+}
